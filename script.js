b0580bc0f5f80f7a3a5f8d542a1b997aa42e2ed8
Mattheus <mattheus.eduardo26@gmail.com>
2024-02-01 21:02:57 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 /* Desenvolva seu código abaixo */
-console.log("minha primeira entrega")
+function playRockPaperScissor (player1, player2){
+    if (player1 == "Pedra" && player2 == "Pedra"){    
+        return "Empate!"
+    }else if (player1 == "Papel" && player2 == "Papel"){
+        return "Empate!"
+        
+    }else if (player1 == "Tesoura" && player2 == "Tesoura"){
+        return "Empate!"
+        
+    }else if (player1 == "Pedra" && player2 == "Tesoura"){
+        return "Jogador 1 venceu!"
+        
+    }else if (player1 == "Pedra" && player2 == "Papel"){
+        return "Jogador 2 venceu!"
+        
+    }else if (player1 == "Papel" && player2 == "Pedra"){
+        return "Jogador 1 venceu!"
+        
+    }else if (player1 == "Papel" && player2 == "Tesoura"){
+        return "Jogador 2 venceu!"
+        
+    }else if (player1 == "Tesoura" && player2 == "Papel"){
+        return "Jogador 1 venceu!"
+        
+    }else if (player1 == "Tesoura" && player2 == "Pedra"){
+        return "Jogador 2 venceu!"
+        
+    }
+}
+
+console.log(playRockPaperScissor)
